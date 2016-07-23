06d33754590c40f59ed95f54dabdaa64b0a970da
Chris Lunsford <chris@cmlccie.com>
2016-07-23 02:10:39 -0400

    Initial CiscoSparkAPI 'package class'
    Create the initial CiscoSparkAPI class.  This is the main package
    class.  It will contain a single RestSession object per instance and
    wrapper and expose all of the component APIs as a single hierarchal
    tree.

--- ciscosparkapi/__init__.py
+++ ciscosparkapi/__init__.py
@@ -1,2 +1,31 @@
 import exceptions
-import restsession
+from restsession import RestSession
+from api.rooms import Room, RoomsAPI
+
+
+class CiscoSparkAPI(object):
+    """Cisco Spark API wrapper class."""
+
+    def __init__(self, access_token, base_url=None, timeout=None):
+        # Process args
+        assert isinstance(access_token, basestring)
+        # Process kwargs
+        session_args = {}
+        if base_url:  session_args['base_url'] = base_url
+        if timeout:  session_args['timeout'] = timeout
+        # Create API session
+        self.session = RestSession(access_token, **session_args)
+        # Setup Spark API wrappers
+        self.rooms = RoomsAPI(self.session)
+
+    @property
+    def access_token(self):
+        return self.session.access_token
+
+    @property
+    def base_url(self):
+        return self.session.base_url
+
+    @property
+    def timeout(self):
+        return self.session.timeout
