d5a2f1ba0b018fda28510524b87b609c0e059b28
Chris Lunsford <chris@cmlccie.com>
2017-03-06 21:20:18 -0500

    Setup test-account email generation fixtures
    Add an test-account email address generating fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-
 """pytest configuration and top-level fixtures."""
 
+
+import string
+
 import pytest
-
-
-EMAIL_ADDRESSES = [
-    'test98@cmlccie.com',
-    'test99@cmlccie.com',
-]
 
 
 pytest_plugins = [
@@ -21,11 +17,48 @@ pytest_plugins = [
     'tests.api.test_teammemberships',
     'tests.api.test_teams',
     'tests.api.test_webhooks',
+    'tests.api.test_organizations',
+    'tests.api.test_licenses',
+    'tests.api.test_roles',
 ]
+
+
+TEST_DOMAIN = "cmlccie.com"
+
+email_template = string.Template("test${number}@" + TEST_DOMAIN)
+_email_addresses = []
+
+
+# Helper Functions
+def new_email_generator():
+    i = 0
+    while True:
+        email_address = email_template.substitute(number=i)
+        _email_addresses.append(email_address)
+        i += 1
+        yield email_address
 
 
 # pytest Fixtures
 
 @pytest.fixture(scope="session")
 def email_addresses():
-    return EMAIL_ADDRESSES
+    return _email_addresses
+
+
+@pytest.fixture(scope="session")
+def get_email_addresses():
+    def inner_function(num):
+        if len(email_addresses) < num:
+            for i in range(num - len(email_addresses)):
+                new_email_address()
+        return email_addresses[:num]
+    return inner_function
+
+
+@pytest.fixture(scope="session")
+def get_new_email_address():
+    generator = new_email_generator()
+    def inner_function():
+        return generator.next()
+    return inner_function
