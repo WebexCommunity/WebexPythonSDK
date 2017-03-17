577ff33ffa0bf04d465e35812f476d7f5b9c79ad
Chris Lunsford <chris@cmlccie.com>
2017-03-17 15:46:20 +0100

    Initial TeamsAPI tests

--- tests/api/test_teams.py
+++ tests/api/test_teams.py
@@ -3,6 +3,8 @@
 """pytest Team functions, fixtures and tests."""
 
 
+import itertools
+
 import pytest
 
 import ciscosparkapi
@@ -15,8 +17,31 @@ def create_team(api, name):
     return api.teams.create(name)
 
 
+def get_team_details_by_id(self, api, team_id):
+    team = api.teams.get(team_id)
+    return team
+
+
 def delete_team(api, team):
     api.teams.delete(team.id)
+
+
+def is_valid_team(obj):
+    return isinstance(obj, ciscosparkapi.Team) and obj.id is not None
+
+
+def are_valid_teams(iterable):
+    return all([is_valid_team(obj) for obj in iterable])
+
+
+def team_exists(api, team):
+    try:
+        get_team_details_by_id(api, team.id)
+    except ciscosparkapi.SparkApiError:
+        return False
+    else:
+        return True
+
 
 
 # pytest Fixtures
@@ -26,3 +51,71 @@ def team(api):
     team =  create_team(api, create_string("Team"))
     yield team
     delete_team(api, team)
+
+
+@pytest.fixture(scope="session")
+def teams_list(api, team):
+    return list(api.teams.list())
+
+
+@pytest.fixture
+def temp_team(api):
+    team = create_team(api, create_string("Team"))
+
+    yield team
+
+    if team_exists(api, team):
+        delete_team(api, team)
+
+
+@pytest.fixture
+def add_teams(api):
+    teams = []
+    
+    def inner(num_teams):
+        for i in range(num_teams):
+            teams.append(create_team(api, create_string("Additional Team")))
+        return teams
+    
+    yield inner
+    
+    for team in teams:
+        delete_team(api, team)
+
+
+# Tests
+
+class TestTeamsAPI(object):
+    """Test TeamsAPI methods."""
+
+    def test_create_team(self, team):
+        assert is_valid_team(team)
+
+    def test_get_team_details(self, api, team):
+        team = get_team_details_by_id(team.id)
+        assert is_valid_team(team)
+
+    def test_update_team_name(self, api, team):
+        new_name = create_string("Updated Team")
+        team = api.teams.update(team.id, name=new_name)
+        assert is_valid_team(team)
+        assert team.title == new_name
+
+    def test_delete_team(self, api, temp_team):
+        api.teams.delete(temp_team.id)
+        assert not team_exists(api, temp_team)
+
+    def test_list_teams(self, api, teams_list):
+        assert len(teams_list) > 0
+        assert are_valid_teams(teams_list)
+
+    def test_list_teams_with_paging(self, api, teams_list, add_teams):
+        page_size = 1
+        pages = 3
+        num_teams = pages * page_size
+        if len(teams_list) < num_teams:
+            add_teams(num_teams - len(teams_list))
+        teams = api.teams.list(max=page_size)
+        teams_list = list(itertools.islice(teams, num_teams))
+        assert len(teams_list) == num_teams
+        assert are_valid_teams(teams_list)
