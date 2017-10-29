2544be02dbff1f50dd1e11f66e1c77574c0c8dda
Chris Lunsford <chris@cmlccie.com>
2017-10-28 21:22:28 -0400

    Refactor Messages
    Apply structural changes to the MessagesAPI and ensure all parameters are up-to-date.

--- ciscosparkapi/api/messages.py
+++ ciscosparkapi/api/messages.py
@@ -3,8 +3,8 @@
 
 Classes:
     Message: Models a Spark 'message' JSON object as a native Python object.
-    MessagesAPI: Wrappers the Cisco Spark Messages-API and exposes the API
-        calls as Python method calls that return native Python objects.
+    MessagesAPI: Wraps the Cisco Spark Messages-API and exposes the APIs as
+        native Python methods that return native Python objects.
 
 """
 
@@ -21,10 +21,11 @@ from past.builtins import basestring
 
 from requests_toolbelt import MultipartEncoder
 
-from ciscosparkapi.exceptions import ciscosparkapiException
 from ciscosparkapi.restsession import RestSession
 from ciscosparkapi.sparkdata import SparkData
 from ciscosparkapi.utils import (
+    check_type,
+    dict_from_items_with_values,
     generator_container,
     is_web_url,
     is_local_file,
@@ -42,10 +43,10 @@ class Message(SparkData):
     """Model a Spark 'message' JSON object as a native Python object."""
 
     def __init__(self, json):
-        """Init a new Message data object from a JSON dictionary or string.
+        """Initialize a Message data object from a dictionary or JSON string.
 
         Args:
-            json(dict, basestring): Input JSON object.
+            json(dict, basestring): Input dictionary or JSON string.
 
         Raises:
             TypeError: If the input object is not a dictionary or string.
@@ -55,64 +56,65 @@ class Message(SparkData):
 
     @property
     def id(self):
-        return self._json['id']
+        """The message's unique ID."""
+        return self._json.get('id')
 
     @property
     def roomId(self):
-        return self._json['roomId']
+        """The ID of the room."""
+        return self._json.get('roomId')
 
     @property
     def roomType(self):
-        return self._json['roomType']
-
-    @property
-    def toPersonId(self):
-        """Optional attribute; returns None if not present."""
-        return self._json.get('toPersonId')
-
-    @property
-    def toPersonEmail(self):
-        """Optional attribute; returns None if not present."""
-        return self._json.get('toPersonEmail')
+        """The type of room (i.e. 'group', 'direct' etc.)."""
+        return self._json.get('roomType')
 
     @property
     def text(self):
-        """Optional attribute; returns None if not present."""
+        """The message, in plain text."""
         return self._json.get('text')
 
     @property
-    def markdown(self):
-        """Optional attribute; returns None if not present."""
-        return self._json.get('markdown')
-
-    @property
     def files(self):
-        """Optional attribute; returns None if not present."""
+        """Files attached to the the message (list of URLs)."""
         return self._json.get('files')
 
     @property
     def personId(self):
-        return self._json['personId']
+        """The person ID of the sender."""
+        return self._json.get('personId')
 
     @property
     def personEmail(self):
-        return self._json['personEmail']
+        """The email address of the sender."""
+        return self._json.get('personEmail')
 
     @property
-    def created(self):
-        return self._json['created']
+    def markdown(self):
+        """The message, in markdown format."""
+        return self._json.get('markdown')
+
+    @property
+    def html(self):
+        """The message, in HTML format."""
+        return self._json.get('html')
 
     @property
     def mentionedPeople(self):
-        """Optional attribute; returns None if not present."""
+        """The list of IDs of people mentioned in the message."""
         return self._json.get('mentionedPeople')
+
+    @property
+    def created(self):
+        """The date and time the message was created."""
+        return self._json.get('created')
 
 
 class MessagesAPI(object):
     """Cisco Spark Messages-API wrapper class.
 
-    Wrappers the Cisco Spark Messages-API and exposes the API calls as Python
-    method calls that return native Python objects.
+    Wraps the Cisco Spark Messages-API and exposes the APIs as native Python
+    methods that return native Python objects.
 
     """
 
@@ -133,12 +135,12 @@ class MessagesAPI(object):
 
     @generator_container
     def list(self, roomId, mentionedPeople=None, before=None,
-             beforeMessage=None, max=None):
-        """List all messages in a room.
+             beforeMessage=None, max=None, **request_parameters):
+        """Lists all messages in a room.
 
-        If present, includes the associated media content attachment for each
-        message.  The list sorts the messages in descending order by creation
-        date.
+        Each message will include content attachments if present.
+
+        The list API sorts the messages in descending order by creation date.
 
         This method supports Cisco Spark's implementation of RFC5988 Web
         Linking to provide pagination support.  It returns a generator
@@ -151,179 +153,170 @@ class MessagesAPI(object):
         container.
 
         Args:
-            roomId(basestring): List messages for the room with roomId.
-            mentionedPeople(basestring): List messages for a person, by
-                personId or me.
-            before(basestring): List messages sent before a date and time,
-                in ISO8601 format
+            roomId(basestring): List messages for a room, by ID.
+            mentionedPeople(basestring): List messages where the caller is
+                mentioned by specifying "me" or the caller `personId`.
+            before(basestring): List messages sent before a date and time, in
+                ISO8601 format.
             beforeMessage(basestring): List messages sent before a message,
-                by message ID
-            max(int): Limit the maximum number of messages returned from the
-                Spark service per request.
+                by ID.
+            max(int): Limit the maximum number of items returned from the Spark
+                service per request.
+            **request_parameters: Additional request parameters (provides
+                support for parameters that may be added in the future).
 
         Returns:
-            GeneratorContainer: When iterated, the GeneratorContainer, yields
-                the messages returned by the Cisco Spark query.
+            GeneratorContainer: A GeneratorContainer which, when iterated,
+                yields the messages returned by the Cisco Spark query.
 
         Raises:
-            AssertionError: If the parameter types are incorrect.
+            TypeError: If the parameter types are incorrect.
             SparkApiError: If the Cisco Spark cloud returns an error.
 
         """
-        # Process args
-        assert isinstance(roomId, basestring)
-        assert mentionedPeople is None or isinstance(mentionedPeople, list)
-        assert before is None or isinstance(before, basestring)
-        assert beforeMessage is None or isinstance(beforeMessage, basestring)
-        assert max is None or isinstance(max, int)
-        params = {}
-        params['roomId'] = roomId
-        if mentionedPeople:
-            params['mentionedPeople'] = mentionedPeople
-        if before:
-            params['before'] = before
-        if beforeMessage:
-            params['beforeMessage'] = beforeMessage
-        if max:
-            params['max'] = max
+        check_type(roomId, basestring, may_be_none=False)
+        check_type(mentionedPeople, basestring)
+        check_type(before, basestring)
+        check_type(beforeMessage, basestring)
+        check_type(max, int)
+
+        params = dict_from_items_with_values(
+            request_parameters,
+            roomId=roomId,
+            mentionedPeople=mentionedPeople,
+            before=before,
+            beforeMessage=beforeMessage,
+            max=max,
+        )
+
         # API request - get items
         items = self._session.get_items('messages', params=params)
+
         # Yield Message objects created from the returned items JSON objects
         for item in items:
             yield Message(item)
 
     def create(self, roomId=None, toPersonId=None, toPersonEmail=None,
-               text=None, markdown=None, files=None):
-        """Posts a message to a room.
+               text=None, markdown=None, files=None, **request_parameters):
+        """Posts a message, and optionally a attachment, to a room.
 
-        Posts a message, and optionally, a media content attachment, to a room.
-
-        You must specify either a roomId, toPersonId or toPersonEmail when
-        posting a message, and you must supply some message content (text,
-        markdown, files).
+        The files parameter is a list, which accepts multiple values to allow
+        for future expansion, but currently only one file may be included with
+        the message.
 
         Args:
             roomId(basestring): The room ID.
             toPersonId(basestring): The ID of the recipient when sending a
                 private 1:1 message.
-            toPersonEmail(basestring): The email address of the recipient
-                when sending a private 1:1 message.
-            text(basestring): The message, in plain text. If markdown is
+            toPersonEmail(basestring): The email address of the recipient when
+                sending a private 1:1 message.
+            text(basestring): The message, in plain text. If `markdown` is
                 specified this parameter may be optionally used to provide
-                alternate text forUI clients that do not support rich text.
+                alternate text for UI clients that do not support rich text.
             markdown(basestring): The message, in markdown format.
-            files(list): A list containing local paths or URL references for
-                the message attachment(s).  The files attribute currently only
-                takes a list containing one (1) filename or URL as an input.
-                This is a Spark API limitation that may be lifted at a later
-                date.
+            files(list): A list of public URL(s) or local path(s) to files to
+                be posted into the room. Only one file is allowed per message.
+                Uploaded files are automatically converted into a format that
+                all Spark clients can render.
+            **request_parameters: Additional request parameters (provides
+                support for parameters that may be added in the future).
 
         Returns:
-            Message: With the details of the created message.
+            Message: A Message object with the details of the created message.
 
         Raises:
-            AssertionError: If the parameter types are incorrect.
-            ciscosparkapiException: If the required arguments are not
-                specified.
+            TypeError: If the parameter types are incorrect.
             SparkApiError: If the Cisco Spark cloud returns an error.
+            ValueError: If the files parameter is a list of length > 1, or if
+                the string in the list (the only element in the list) does not
+                contain a valid URL or path to a local file.
 
         """
-        # Process args
-        assert roomId is None or isinstance(roomId, basestring)
-        assert toPersonId is None or isinstance(toPersonId, basestring)
-        assert toPersonEmail is None or isinstance(toPersonEmail, basestring)
-        assert text is None or isinstance(text, basestring)
-        assert markdown is None or isinstance(markdown, basestring)
-        assert files is None or isinstance(files, list)
-        post_data = {}
-        # Where is message to be posted?
-        if roomId:
-            post_data['roomId'] = roomId
-        elif toPersonId:
-            post_data['toPersonId'] = toPersonId
-        elif toPersonEmail:
-            post_data['toPersonEmail'] = toPersonEmail
-        else:
-            error_message = "You must specify a roomId, toPersonId, or " \
-                            "toPersonEmail to which you want to post a new " \
-                            "message."
-            raise ciscosparkapiException(error_message)
-        # Ensure some message 'content' is provided.
-        if not text and not markdown and not files:
-            error_message = "You must supply some message content (text, " \
-                            "markdown, files) when posting a message."
-            raise ciscosparkapiException(error_message)
-        # Process the content.
-        if text:
-            post_data['text'] = text
-        if markdown:
-            post_data['markdown'] = markdown
-        upload_local_file = False
+        check_type(roomId, basestring)
+        check_type(toPersonId, basestring)
+        check_type(toPersonEmail, basestring)
+        check_type(text, basestring)
+        check_type(markdown, basestring)
+        check_type(files, list)
         if files:
-            if len(files) > 1:
-                error_message = "The files attribute currently only takes a " \
-                                "list containing one (1) filename or URL as " \
-                                "an input.  This is a Spark API limitation " \
-                                "that may be lifted at a later date."
-                raise ciscosparkapiException(error_message)
-            if is_web_url(files[0]):
-                post_data['files'] = files
-            elif is_local_file(files[0]):
-                upload_local_file = True
-                post_data['files'] = open_local_file(files[0])
-            else:
-                error_message = "The provided files argument does not " \
-                                "contain a valid URL or local file path."
-                raise ciscosparkapiException(error_message)
+            if len(files) != 1:
+                raise ValueError("The length of the `files` list is greater "
+                                 "than one (1). The files parameter is a "
+                                 "list, which accepts multiple values to "
+                                 "allow for future expansion, but currently "
+                                 "only one file may be included with the "
+                                 "message.")
+            check_type(files[0], basestring)
+
+        post_data = dict_from_items_with_values(
+            request_parameters,
+            roomId=roomId,
+            toPersonId=toPersonId,
+            toPersonEmail=toPersonEmail,
+            text=text,
+            markdown=markdown,
+            files=files,
+        )
+
         # API request
-        if upload_local_file:
+        if not files or is_web_url(files[0]):
+            # Standard JSON post
+            json_data = self._session.post('messages', json=post_data)
+
+        elif is_local_file(files[0]):
+            # Multipart MIME post
             try:
+                post_data['files'] = open_local_file(files[0])
                 multipart_data = MultipartEncoder(post_data)
                 headers = {'Content-type': multipart_data.content_type}
-                json_obj = self._session.post('messages',
-                                              data=multipart_data,
-                                              headers=headers)
+                json_data = self._session.post('messages',
+                                               headers=headers,
+                                               data=multipart_data)
             finally:
                 post_data['files'].file_object.close()
+
         else:
-            json_obj = self._session.post('messages', json=post_data)
+            raise ValueError("The `files` parameter does not contain a vaild "
+                             "URL or path to a local file.")
+
         # Return a Message object created from the response JSON data
-        return Message(json_obj)
+        return Message(json_data)
 
     def get(self, messageId):
         """Get the details of a message, by ID.
 
         Args:
-            messageId(basestring): The messageId of the message.
+            messageId(basestring): The ID of the message to be retrieved.
 
         Returns:
-            Message: With the details of the requested message.
+            Message: A Message object with the details of the requested
+                message.
 
         Raises:
-            AssertionError: If the parameter types are incorrect.
+            TypeError: If the parameter types are incorrect.
             SparkApiError: If the Cisco Spark cloud returns an error.
 
         """
-        # Process args
-        assert isinstance(messageId, basestring)
+        check_type(messageId, basestring, may_be_none=False)
+
         # API request
-        json_obj = self._session.get('messages/' + messageId)
+        json_data = self._session.get('messages/' + messageId)
+
         # Return a Message object created from the response JSON data
-        return Message(json_obj)
+        return Message(json_data)
 
     def delete(self, messageId):
         """Delete a message.
 
         Args:
-            messageId(basestring): The messageId of the message to be
-                deleted.
+            messageId(basestring): The ID of the message to be deleted.
 
         Raises:
-            AssertionError: If the parameter types are incorrect.
+            TypeError: If the parameter types are incorrect.
             SparkApiError: If the Cisco Spark cloud returns an error.
 
         """
-        # Process args
-        assert isinstance(messageId, basestring)
+        check_type(messageId, basestring, may_be_none=False)
+
         # API request
         self._session.delete('messages/' + messageId)
