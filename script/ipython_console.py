6299413f105fecbc6f42d31f47110e5a85558e34
Chris Lunsford <chris@cmlccie.com>
2018-05-28 06:55:06 -0400

    Switch to using Scripts to Rule Them All instead of Makefile
    Create a `script/` directory to contain scripts for common developer workflow scripts, and remove the project's Makefile.  Include a `script/README.md` with information on the workflow scripts.

---
+++ script/ipython_console.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""IPython Project Console.
+
+Used to interactively work with the main package contents in IPython.
+"""
+
+
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
+from builtins import *
+
+import ciscosparkapi
+
+
+__copyright__ = "Copyright (c) 2016-2018 Cisco and/or its affiliates."
+__license__ = "MIT"
+
+
+api = ciscosparkapi.CiscoSparkAPI()
