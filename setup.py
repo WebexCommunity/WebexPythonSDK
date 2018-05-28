7e7d44207ff10295356dd3d42babe83cba2eaac5
Chris Lunsford <chris@cmlccie.com>
2018-05-28 09:40:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,68 +2,92 @@
 """A setuptools based setup module."""
 
 
+import os
 from codecs import open
-from os import path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 import versioneer
 
 
-__author__ = "Chris Lunsford"
-__author_email__ = "chrlunsf@cisco.com"
 __copyright__ = "Copyright (c) 2016-2018 Cisco and/or its affiliates."
 __license__ = "MIT"
 
 
-# Get the long description from the README file
-current_path = path.abspath(path.dirname(__file__))
-with open(path.join(current_path, 'README.rst'), encoding='utf-8') as f:
+PACKAGE_NAME = 'webexteamsdk'
+
+PACKAGE_KEYWORDS = [
+    'cisco',
+    'webex',
+    'teams',
+    'spark',
+    'python',
+    'api',
+    'sdk',
+    'enterprise',
+    'messaging',
+]
+
+PACKAGE_CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Intended Audience :: Telecommunications Industry',
+    'Intended Audience :: Education',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Communications',
+    'Topic :: Communications :: Chat'
+]
+
+INSTALLATION_REQUIREMENTS = [
+    'future',
+    'requests>=2.4.2',
+    'requests-toolbelt',
+]
+
+
+project_root = os.path.abspath(os.path.dirname(__file__))
+
+
+# Get package metadata
+metadata = {}
+with open(os.path.join(project_root, PACKAGE_NAME, '_metadata.py')) as f:
+    exec(f.read(), metadata)
+
+
+# Get the long description from the project's README.rst file
+with open(os.path.join(project_root, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
 setup(
-    name='webexteamsdk',
+    name=PACKAGE_NAME,
 
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 
-    description='Simple, lightweight, scalable Python API wrapper for the '
-                'Cisco Spark APIs',
+    description=metadata['__description__'],
     long_description=long_description,
 
-    url='https://github.com/CiscoDevNet/webexteamsdk',
-    download_url="https://pypi.python.org/pypi/webexteamsdk",
+    url=metadata['__url__'],
+    download_url=metadata['__download_url__'],
 
-    author=__author__,
-    author_email=__author_email__,
+    author=metadata['__author__'],
+    author_email=metadata['__author_email__'],
 
-    license=__license__+"; "+__copyright__,
+    license=metadata['__license__'] + '; ' + metadata['__copyright__'],
 
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Telecommunications Industry',
-        'Intended Audience :: Education',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Communications',
-        'Topic :: Communications :: Chat'
-    ],
+    classifiers=PACKAGE_CLASSIFIERS,
 
-    keywords='cisco spark api enterprise messaging',
+    keywords=" ".join(PACKAGE_KEYWORDS),
 
-    packages=find_packages(include=['webexteamsdk', 'webexteamsdk.*']),
+    packages=find_packages(include=[PACKAGE_NAME, PACKAGE_NAME + '.*']),
 
-    install_requires=[
-            'future',
-            'requests>=2.4.2',
-            'requests-toolbelt',
-    ],
+    install_requires=INSTALLATION_REQUIREMENTS,
 )
---
+++ webexteamsdk/_metadata.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Package metadata."""
+
+
+__title__ = 'webexteamsdk'
+__description__ = 'Community-developed Python SDK for the Webex Teams APIs'
+__url__ = 'https://github.com/CiscoDevNet/webexteamsdk'
+__download_url__ = 'https://pypi.python.org/pypi/webexteamsdk'
+__author__ = 'Chris Lunsford'
+__author_email__ = 'chrlunsf@cisco.com'
+__copyright__ = "Copyright (c) 2016-2018 Cisco Systems, Inc."
+__license__ = "MIT"
