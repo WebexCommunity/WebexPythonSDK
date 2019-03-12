d074e9dab2bf1a975aecfa32b269a6d19032cea7
Neidinger, Marcel <mneiding@cisco.com>
2019-03-12 10:21:35 +0100

    Added type checks for guest issuer parameters

--- webexteamssdk/api/guest_issuer.py
+++ webexteamssdk/api/guest_issuer.py
@@ -95,6 +95,11 @@ class GuestIssuerAPI(object):
             TypeError: If the parameter types are incorrect
             ApiError: If the webex teams cloud returns an error.
         """
+        check_type(subject, basestring)
+        check_type(displayName, basestring)
+        check_type(issuerToken, basestring)
+        check_type(expiration, basestring)
+        check_type(secret, basestring)
 
         payload = {
             "sub": subject,
