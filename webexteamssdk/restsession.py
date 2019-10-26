467f9a9eeeba56c2854da9c568b672d5fd84fdf7
Chris Lunsford <chrlunsf@cisco.com>
2019-10-26 08:18:34 -0400

    Raise a more descriptive ValueError if single_request_timeout is less than 0

--- webexteamssdk/restsession.py
+++ webexteamssdk/restsession.py
@@ -162,7 +162,8 @@ class RestSession(object):
     def single_request_timeout(self, value):
         """The timeout (seconds) for a single HTTP REST API request."""
         check_type(value, int, optional=True)
-        assert value is None or value > 0
+        if value is not None and value <= 0:
+            raise ValueError("single_request_timeout must be positive integer")
         self._single_request_timeout = value
 
     @property
