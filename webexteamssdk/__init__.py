56912bbc52696a5d38f15691e5a5f49968e87b75
Chris Lunsford <chrlunsf@cisco.com>
2020-07-09 13:50:24 -0400

    Explicitly import the metadata attributes

--- webexteamssdk/__init__.py
+++ webexteamssdk/__init__.py
@@ -33,7 +33,10 @@ from __future__ import (
 import logging
 
 import webexteamssdk.models.cards as cards
-from ._metadata import *
+from ._metadata import (
+    __author__, __author_email__, __copyright__, __description__,
+    __download_url__, __license__, __title__, __url__, __version__,
+)
 from .api import WebexTeamsAPI
 from .exceptions import (
     AccessTokenError, ApiError, ApiWarning, MalformedResponse, RateLimitError,
