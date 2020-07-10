ede0acd484b119420c8a99bb6dc39d8c8eb747cc
Chris Lunsford <chrlunsf@cisco.com>
2020-07-10 17:12:27 -0400

    Update Messages API
    - Add the parentId parameter to the messages.list() method.
    - Add the messages.list_direct() API endpoint.
    - Update the message data model attributes.

--- webexteamssdk/models/mixins/message.py
+++ webexteamssdk/models/mixins/message.py
@@ -41,12 +41,17 @@ class MessageBasicPropertiesMixin(object):
     @property
     def id(self):
         """The unique identifier for the message."""
-        return self._json_data.get('id')
+        return self._json_data.get("id")
+
+    @property
+    def parentId(self):
+        """The unique identifier for the parent message."""
+        return self._json_data.get("parentId")
 
     @property
     def roomId(self):
         """The room ID of the message."""
-        return self._json_data.get('roomId')
+        return self._json_data.get("roomId")
 
     @property
     def roomType(self):
@@ -56,57 +61,79 @@ class MessageBasicPropertiesMixin(object):
             `direct`: 1:1 room
             `group`: Group room
         """
-        return self._json_data.get('roomType')
+        return self._json_data.get("roomType")
+
+    @property
+    def toPersonId(self):
+        """The person ID of the recipient when sending a 1:1 message."""
+        return self._json_data.get("toPersonId")
+
+    @property
+    def toPersonEmail(self):
+        """The email address of the recipient when sending a 1:1 message."""
+        return self._json_data.get("toPersonEmail")
 
     @property
     def text(self):
         """The message, in plain text."""
-        return self._json_data.get('text')
+        return self._json_data.get("text")
 
     @property
     def markdown(self):
         """The message, in Markdown format."""
-        return self._json_data.get('markdown')
+        return self._json_data.get("markdown")
 
     @property
     def html(self):
-        """The message, in HTML format."""
-        return self._json_data.get('html')
+        """The text content of the message, in HTML format.
+
+        This read-only property is used by the Webex Teams clients.
+        """
+        return self._json_data.get("html")
 
     @property
     def files(self):
         """Public URLs for files attached to the message."""
-        return self._json_data.get('files')
+        return self._json_data.get("files")
 
     @property
     def personId(self):
         """The person ID of the message author."""
-        return self._json_data.get('personId')
+        return self._json_data.get("personId")
 
     @property
     def personEmail(self):
         """The email address of the message author."""
-        return self._json_data.get('personEmail')
+        return self._json_data.get("personEmail")
 
     @property
     def mentionedPeople(self):
         """People IDs for anyone mentioned in the message."""
-        return self._json_data.get('mentionedPeople')
+        return self._json_data.get("mentionedPeople")
 
     @property
     def mentionedGroups(self):
         """Group names for the groups mentioned in the message."""
-        return self._json_data.get('mentionedGroups')
+        return self._json_data.get("mentionedGroups")
 
     @property
-    def parentId(self):
-        """The unique identifier for the parent message."""
-        return self._json_data.get('parentId')
+    def attachments(self):
+        """Message content attachments attached to the message."""
+        return self._json_data.get("attachments")
 
     @property
     def created(self):
         """The date and time the message was created."""
-        created = self._json_data.get('created')
+        created = self._json_data.get("created")
+        if created:
+            return WebexTeamsDateTime.strptime(created)
+        else:
+            return None
+
+    @property
+    def updated(self):
+        """The date and time the message was created."""
+        created = self._json_data.get("updated")
         if created:
             return WebexTeamsDateTime.strptime(created)
         else:
