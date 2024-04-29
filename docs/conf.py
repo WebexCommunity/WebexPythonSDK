9fba8b78f05707cb5305ace4c9f4613087f23e9f
Chris Lunsford <cm@lunsford.io>
2024-04-29 07:20:11 -0400

    Update language setting in conf.py to "en" and update intersphinx_mapping URL

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ copyright = "Copyright (c) 2016-2024 Cisco and/or its affiliates."
 author = "Chris Lunsford"
 version = get_versions()["version"]
 release = get_versions()["version"]
-language = None
+language = "en"
 
 
 extensions = [
@@ -288,4 +288,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
